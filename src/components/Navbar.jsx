15da52404ac3829f50b03a889e4ab68e4de17884
teodorajov <teodora9jovanovic@gmail.com>
2022-01-29 23:49:02 +0100

    Kreiran navbar, putanje i podaci za overview

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark sticky-top bg-dark">
+      <NavLink className="navbar-brand" to='/' >Zodiac </NavLink>
+
+      <div className="navbar-collapse collapse">
+        <ul className="navbar-nav mx-auto">
+          <li className="nav-item mx-auto">
+            <NavLink to='/' className='nav-link'>Daily horoscope</NavLink>
+          </li>
+          <li className="nav-item mx-auto">
+            <NavLink to='/overview' className='nav-link'>Zodiac signs overview</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
